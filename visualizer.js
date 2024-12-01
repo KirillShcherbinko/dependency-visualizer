39e3da621bfbb13ed75ca57a573c6ea47a7e5fee
Kirill Shcherbinko <kirill.sherbinko@gmail.com>
2024-12-01 15:39:05 +0300

    added function for getting dependencies

--- visualizer.js
+++ visualizer.js
@@ -6,7 +6,7 @@ function readKeys() {
     const args = process.argv.slice(2);
 
     // Проверка на число введённых аргументов
-    if (args.length > 4) {
+    if (args.length > 5) {
         console.log("Введено слишком много аргументов");
         process.exit(1);
     } else if (args.length < 3) {
@@ -19,44 +19,120 @@ function readKeys() {
         graphProgramPath: args[0],
         packageName: args[1],
         depth: args[2],
-        version: args[3] || "latest"
+        version: args[3] || "latest",
+        targetFramework: args[4] || ".NETStandard2.0"
     };
 
     return keys;
 }
 
-async function getLatestVersionPackage(packageName) {
-    // Ссылка на сам пакет
-    const allPackageUrl = `https://api.nuget.org/v3/registration5-semver1/${packageName.toLowerCase()}/index.json`;
+function isVersionCompatible(version, range) {}
+
+function findVersion(versions, packageVersion) {
+    // Если нужно найти последнюю версию
+    if (packageVersion === "latest")
+        return versions[versions.length - 1];
+
+    // Если нужно найти конкретную версию
+    return versions.includes(packageVersion) ? packageVersion : "not found";
+}
+
+async function getPackageVersion(packageName, packageVersion) {
+    // Ссылка на все версии пакета
+    const versionsUrl = `https://api.nuget.org/v3-flatcontainer/${packageName.toLowerCase()}/index.json`;
 
     // Запрос на получение версии
-    return axios.get(allPackageUrl)
+    return axios.get(versionsUrl)
         .then(res => {
-            // Поучаем данные о версии
-            const data = res.data.items;
-            const latestVersion = data[data.length - 1].upper;
+            // Получение версии
+            const versions = res.data.versions;
+            const version = findVersion(versions, packageVersion);
+  
+            // Проверка наличия версии
+            if (version === "not found") {
+                throw new Error(`версия ${packageVersion} пакета не найдена`);
+            }
 
-            return latestVersion;
+            // Возврат найденной версии
+            return version;
         })
-
-        .catch(() => {
-            console.error(`Ошибка при получении пакета ${packageName}`);
+        .catch((err) => {
+            console.error(`Ошибка при получении версии пакета ${packageName}: ${err.message}`);
         })
 }
 
-async function fetchPackageData(packageName, packageVersion) {
-    const packageUrl = `https://api.nuget.org/v3/catalog0/data/2023.03.08.07.46.17/${packageName}.${packageVersion}.json`
-    console.log(packageUrl)
+async function findDependencies(dependenciesUrl, packageTargetFramework) {
+    return axios.get(dependenciesUrl)
+        .then(res => {
+            const packageData = res.data.dependencyGroups.find(element => element.targetFramework === packageTargetFramework);
+            return packageData.dependencies || [];
+        })
+        .catch(err => {
+            console.error(`Ошибка при получении зависисмостей: ${err.message}`);
+        })
+}
+
+async function fetchDependencies(packageName, packageVersion, packageTargetFramework) {
+    // Ссылка на зависисмости пакета
+    const packageUrl = `https://api.nuget.org/v3/registration5-gz-semver2/${packageName}/${packageVersion}.json`
+
     return axios.get(packageUrl)
         .then(res => {
-            const dependencies = res.data.dependencyGroups  || [];
+            // Получаем массив зависимостей
+            const dependenciesUrl = res.data.catalogEntry;
+            const dependencies = findDependencies(dependenciesUrl, packageTargetFramework);
             return dependencies;
         })
-        .catch(() => {
-            console.error(`Ошибка при получении пакета ${packageName}`);
+        .catch(err => {
+            console.error(`Ошибка при получении пакета ${packageName}: ${err.message}`);
         })
 }
 
-const url = await getLatestVersionPackage("newtonsoft.json");
-const dependencies = await fetchPackageData("newtonsoft.json", url);
-console.log(dependencies);
+async function getDependencies(packageName, packageVersion, packageTargetFramework, depth, curDepth, graph) {
+    // Проверка на достижение максимальной глубины
+    if (curDepth > depth) {
+        return;
+    }
+
+    // Получение информации о версии
+    packageVersion = await getPackageVersion(packageName, packageVersion);
+
+    // Получение данных о зависисмостях пакета
+    const packageDependencies = await fetchDependencies(packageName, packageVersion, packageTargetFramework);
+
+    if (!packageDependencies) {
+        return;
+    }
+
+    // Добавление нового узла
+    if (!graph.nodes.includes(packageName)) {
+        graph.nodes.push(packageName);
+    }
+
+    if (packageDependencies.length !== 0) {
+        // Создаем массив зависимостей для конкретного пакета
+        if (!graph.dependencies[packageName]) {
+            graph.dependencies[packageName] = [];
+        }
+
+        // Получаем данные о версии
+        for (const dependency of packageDependencies) {
+            graph.dependencies[packageName].push({
+                name: dependency.id,
+                version: await getPackageVersion(dependency.id, dependency.range)
+            });
+
+            // Рекурсивный вызов для зависимостей
+            await getDependencies(dependency.id, dependency.version, packageTargetFramework, depth, curDepth + 1, graph);
+        }
+    }
+}
+
+const graph = {
+    nodes: [],
+    dependencies: {}
+}
+//const version = await getPackageVersion("serilog", "latest");
+//const dependencies = await fetchDependencies("serilog", version, ".NETStandard2.0");
+await getDependencies("newtonsoft.json", "latest", ".NETStandard2.0", 1, 1, graph)
+console.log(graph);
